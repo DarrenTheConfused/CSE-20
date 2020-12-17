97bb41509b8854b46c88abfd9816192add43e792
DarrenTheConfused <daredog101@gmail.com>
2020-12-17 12:44:13 -0800

    Add files via upload

---
+++ Lab-2/Interview.java
@@ -0,0 +1,12 @@
+
+public class Interview {
+
+	public static void main(String[] args) {
+		System.out.print("Person interviewed:\t\t Isabel \n");
+		System.out.print("Isabel's hometown:\t\t Pomona \n");
+		System.out.println("Isabel's major:\t\t\t Computer Science");
+		System.out.println("Isabel's class standing:\t First year \nThis is the second CSE20 lab.");
+	
+	}
+
+}
